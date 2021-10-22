58acda32d9496dfad0c1017e44bca6e6673ad528
Marko Tomljanović <markot@windowslive.com>
2021-10-22 10:33:52 +0200

    Initial commit

---
+++ src/main/java/com/example/demo/controller/service/EmployeeSevice.java
@@ -0,0 +1,11 @@
+package com.example.demo.controller.service;
+
+import java.util.List;
+
+import com.example.demo.model.Employee;
+
+public interface EmployeeSevice {
+
+	List<Employee> getAllEmployees();	 
+
+}
