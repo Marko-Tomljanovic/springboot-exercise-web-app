851910383ac068a7d703bb054d87ba831b0b9717
Marko Tomljanović <markot@windowslive.com>
2021-10-22 14:13:23 +0200

    table, conection, etc
    + dodavanje tablice
    + konekcija baze
    + dodavanje atributa
    +import botstrap-a
    + style tablice
    + etc

--- src/main/java/com/example/demo/controller/service/EmployeeController.java
+++ src/main/java/com/example/demo/controller/service/EmployeeController.java
@@ -1,5 +1,21 @@
 package com.example.demo.controller.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
 public class EmployeeController {
+	
+	@Autowired
+	private EmployeeSevice employeeSevice;
+	
+	//prikaz liste Employee
+	@GetMapping("/")
+	public String viewHomePage(Model model){
+		model.addAttribute("listEmployees", employeeSevice.getAllEmployees());
+		return "index";
+}
 
 }
