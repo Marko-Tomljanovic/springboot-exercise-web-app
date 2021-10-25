ddfbeb3970b19bdb311799d56ae7a23a0a990ad3
Marko Tomljanović <markot@windowslive.com>
2021-10-25 10:26:24 +0200

    proširenje app
    + omogućena izmjena nad employee
    + omogučeno brisanje employee

--- src/main/java/com/example/demo/controller/service/EmployeeController.java
+++ src/main/java/com/example/demo/controller/service/EmployeeController.java
@@ -5,35 +5,54 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.thymeleaf.cache.ICache;
 
 import com.example.demo.model.Employee;
 
 @Controller
 public class EmployeeController {
-	
+
 	@Autowired
 	private EmployeeSevice employeeSevice;
-	
-	//prikaz liste Employee
+
+	// prikaz liste Employee
 	@GetMapping("/")
-	public String viewHomePage(Model model){
+	public String viewHomePage(Model model) {
 		model.addAttribute("listEmployees", employeeSevice.getAllEmployees());
 		return "index";
-}
-	
+	}
+
 	@GetMapping("/showNewEmployeeForm")
 	public String showNewEmployeeForm(Model model) {
-		Employee employee= new Employee();
-		model.addAttribute("employe", employee);
+		Employee employee = new Employee();
+		model.addAttribute("employee", employee);
 		return "new_employee";
 	}
-	
+
 	@PostMapping("/saveEmployee")
-	public String saveEmployee(@ModelAttribute("employee")Employee employee){
+	public String saveEmployee(@ModelAttribute("employee") Employee employee) {
 		employeeSevice.saveEmployee(employee);
+		return "redirect:/";
+
+	}
+
+	@GetMapping("/showFormForUpdate/{id}")
+	public String showFormForUpdate(@PathVariable(value = "id") long id, Model model) {
+
+		Employee employee = employeeSevice.getEmployeeById(id);
+
+		model.addAttribute("employee", employee);
+		return "update_employee";
+	}
+	
+	@GetMapping("/deleteEmployee/{id}")
+	public String deleteEmployee(@PathVariable (value = "id") long id) {
+		
+		this.employeeSevice.deleteEmployeeById(id);
 		return "redirect:/";
 		
 	}
-
+	
 }
--- src/main/java/com/example/demo/controller/service/EmployeeSevice.java
+++ src/main/java/com/example/demo/controller/service/EmployeeSevice.java
@@ -5,8 +5,9 @@ import java.util.List;
 import com.example.demo.model.Employee;
 
 public interface EmployeeSevice {
-
 	List<Employee> getAllEmployees();	 
 	void saveEmployee(Employee employee);
+	Employee getEmployeeById(long id);
+	void deleteEmployeeById(long id); 
 
 }
